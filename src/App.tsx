2fd13394067ebae124b6064704894e8b5f4b941f
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-02-02 11:45:30 -0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, RouteComponentProps } from 'react-router-dom';
-import { uuid } from 'uuidv4';
-import api from './api/news';
-import Header from './components/Header';
-import AddNews from './pages/AddNews';
-import NewsList from './pages/NewsList';
-import NewsDetail from './pages/NewsDetail';
-import EditNews from './pages/EditNews';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import { NewsPost } from './pages/NewsList';
-
-export default function App() {
-
-  const [newsPost, setNewsPost] = useState<NewsPost[]>([]);
-
-  const retrieveNews = async () => {
-    const response = await api.get('/news');
-
-    return response.data;
-  };
-
-  const addNewsHandler = async (news: any) => {
-    const request = {
-      id: uuid(),
-      ...news
-    };
-    const response = await api.post('/news', request);
-    setNewsPost([...newsPost, response.data]);
-  };
-
-  const updateNewsHandler = async (news: any) => {
-    const response = await api.patch(`/news/${news.id}`, news);
-
-    const { id } = response.data;
-    setNewsPost(newsPost.map(news => {
-
-      return news.id === id ? { ...response.data } : news;
-
-    })
-    );
-
-  };
-
-  const removeNewsHandler = async (id: any) => {
-    await api.delete(`/news/${id}`)
-    const currentNewsPost = newsPost.filter((news) => {
-      return news.id !== id;
-    });
-
-    setNewsPost(currentNewsPost);
-  };
-
-  useEffect(() => {
-    const getAllNews = async () => {
-      const allNews = await retrieveNews();
-      if (allNews) setNewsPost(allNews)
-    };
-
-    getAllNews();
-
-  }, []);
-
+function App() {
   return (
-
-    <div>
-      <Router>
-        <Header />
-        <Switch>
-          <Route
-            path='/'
-            exact
-            render={(props) => (
-              <NewsList
-                {...props}
-                newsPost={newsPost}
-                removeNewsHandler={removeNewsHandler}
-              />
-            )}
-          />
-
-          <Route
-            path='/add'
-            render={(props) => (<AddNews {...props} addNewsHandler={addNewsHandler} />)}
-          />
-
-          <Route
-            path='/edit'
-            render={(props: any) => (<EditNews {...props} updateNewsHandler={updateNewsHandler} />)}
-          />
-
-          <Route path='/news/:id' component={NewsDetail} />
-
-        </Switch>
-
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-
   );
 }
 
-
-
-
+export default App;
